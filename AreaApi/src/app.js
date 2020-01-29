849644e6325b3c2ac1e36471ab2f4e18e6437660
Jostyck9 <hugo.berthome@epitech.eu>
2020-01-29 18:52:36 +0100

    adding JWT authentification

--- AreaApi/src/app.js
+++ AreaApi/src/app.js
@@ -1,16 +1,16 @@
 const express = require('express')
-// const userRouter = require('./routers/user')
+const userRouter = require('./routers/user')
 const port = process.env.PORT
-// require('./db/db')
+require('./db/db')
 
 const app = express()
 
-// app.use(express.json())
-// app.use(userRouter)
+app.use(express.json())
+app.use(userRouter)
 
-app.get('/', function(req, res) { // création de la route sous le verbe get
-    res.send('Hello world  ! ') // envoi de hello world a l'utilisateur
-})
+// app.get('/', function(req, res) { // création de la route sous le verbe get
+//     res.send('Hello world  ! ') // envoi de hello world a l'utilisateur
+// })
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
