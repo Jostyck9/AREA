cdec27588ca1d00a014b5b17f7f216defbc88856
Jostyck9 <hugo.berthome@epitech.eu>
2020-01-29 11:53:29 +0100

    adding Areapi in node.js with yarn packet manager

---
+++ AreaApi/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+// const userRouter = require('./routers/user')
+const port = process.env.PORT
+// require('./db/db')
+
+const app = express()
+
+// app.use(express.json())
+// app.use(userRouter)
+
+app.get('/', function(req, res) { // création de la route sous le verbe get
+    res.send('Hello world  ! ') // envoi de hello world a l'utilisateur
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
