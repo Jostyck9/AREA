99136f445faafea1181e8e4ae6ae11945f085e84
Bailloux <esther.bailloux@epitech.eu>
2020-02-03 11:07:01 +0100

    + first addition of routes

---
+++ AreaApi/AreaApi/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AreaApi.Controllers
+{
+    [Route("auth/")]
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> getAuthServices()
+        {
+          //  return authServices;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+
+        }
+
+        [HttpPost]
+        [Route("signup")]
+        public async Task<IActionResult> Signup([FromBody] LoginModel model)
+        {
+
+        }
+
+    }
+}
---
+++ AreaApi/AreaApi/Controllers/ServicesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AreaApi.Controllers
+{
+
+    [Route("Services/")]
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> getServices()
+        {
+            //return list of available services
+        }
+    }
+}
