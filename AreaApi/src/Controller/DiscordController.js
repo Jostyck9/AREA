3072fbf681b7c2ca98aebd86d6eafcc232844b14
Bailloux <esther.bailloux@epitech.eu>
2020-02-18 00:52:08 +0100

    * trying to send & receive msg on discord

--- AreaApi/src/Controller/DiscordController.js
+++ AreaApi/src/Controller/DiscordController.js
@@ -1,33 +1,42 @@
-// const Discord = require('discord.js');
-// const client = new Discord.Client();
-
-// client.on('ready', () => {
-//   console.log(`Logged in as ${client.user.tag}!`);
-// });
-
-// client.on('message', msg => {
-//   if (msg.content === 'ping') {
-//     msg.reply('pong');
-//   }
-// });
-
-// client.login('token');
 require ('dotenv'). Config () ;
 const Discord = require ('discord.js');
 const bot = new Discord.Client ();
 
-// const TOKEN = process.env.TOKEN;
-const TOKEN = "NjY4NzU3MjkyNzAyODkyMDg4.XkJ9ww.bi81572px9jN5qxJ08QBFBiE3tw";
-
+const TOKEN = process.env.DISCORD_BOT_TOKEN;
 bot.login (TOKEN);
 
 bot.on ('ready', () => {
     console.info (`Connecté en tant que $ {bot.user.tag}!`);
 });
 
-bot.on ('message', msg => {
-    si (msg.content === 'ping') {
-        msg.reply ('pong');
-        msg.channel.send ('pong');
+
+jsonA = {"Message":"Hello World", "ToGuild":true, "guild": "TESTAREA", "channel":"test", "user":""};
+
+jsonB = {"Message":"Hello World", "ToGuild":false, "guild": "", "channel":"", "user":"Aphi"};
+
+/**
+ * Send a specified message in Discord
+ * @group Discord - Discord SendMessage Action
+ * @property {JSON} params - Message to be sent and in which channel
+ * @returns {Error}  default - Unexpected error
+ */
+exports.sendMessage = async function (req, res) {
+    //Send a specified message in Discord
+
+    console.info(req.body);
+    var obj = JSON.parse(req.body);
+    if (obj.ToGuild == true) {
+        bot.sendMessage(obj.Message);
+        //send msg to specifi channel ou throw error
+        return;
     }
-});
+    // bot.channels.get(`channelID`).send(`Text`)
+
+    //try to send msg in dm to specified user
+    //     msg.channel.send();
+
+}
+
+exports.receiveMessage = async function (req, res) {
+    //webhook ? ou le bot sur bot.on ('message', msg => {})
+}
