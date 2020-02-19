4d1c4d483c393e7e44b2f19b5ff274ede3fcc055
Romane <romane.bezier@gmail.com>
2020-02-19 09:06:52 +0100

    Delete firebase

--- AreaApp/app/src/main/java/com/example/area/ProfileActivity.kt
+++ AreaApp/app/src/main/java/com/example/area/ProfileActivity.kt
@@ -3,9 +3,6 @@ package com.example.area
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
@@ -23,7 +20,6 @@ class ProfileActivity : AppCompatActivity() {
 
         profileRedirection.setOnClickListener {
             //TODO déconnecter l'utilisateur
-            FirebaseAuth.getInstance().signOut()
             val i = Intent(applicationContext, MainActivity::class.java)
             i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
