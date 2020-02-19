a2b1bab24c8b10877848fdf61c6e458fc53c9e67
Romane <romane.bezier@gmail.com>
2020-02-19 08:39:41 +0100

    Cancel google auth

--- AreaApp/app/src/main/java/com/example/area/MainActivity.kt
+++ AreaApp/app/src/main/java/com/example/area/MainActivity.kt
@@ -3,53 +3,14 @@ package com.example.area
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.Toast
-import com.google.android.gms.auth.api.Auth
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.GoogleApiClient
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    var mGoogleApiClient: GoogleApiClient? = null
-    var mAuthListener: FirebaseAuth.AuthStateListener? = null
-
-    override fun onStart() {
-        super.onStart()
-        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener!!)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        googleButton.setOnClickListener { signIn() }
-
-        mAuthListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
-            if (firebaseAuth.currentUser != null) {
-                startActivity(Intent(this@MainActivity, HomeActivity::class.java))
-            }
-        }
-        val gso =
-            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build()
-        mGoogleApiClient = GoogleApiClient.Builder(this)
-            .enableAutoManage(this) {
-                Toast.makeText(
-                    this@MainActivity,
-                    "Something went wrong",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-            .build()
 
         //Redirection
         val signInRedirection: Button = findViewById(R.id.signInRedirection)
@@ -57,53 +18,5 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
-    }
-
-    private fun signIn() {
-        val signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
-        startActivityForResult(signInIntent, RC_SIGN_IN)
-    }
-
-    public override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ) {
-        super.onActivityResult(resultCode, resultCode, data)
-        if (requestCode == RC_SIGN_IN) {
-            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
-            if (result.isSuccess) {
-                val account = result.signInAccount
-                firebaseAuthWithGoogle(account)
-            } else {
-                Toast.makeText(this, "Auth went wrong", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
-        val credential =
-            GoogleAuthProvider.getCredential(account!!.idToken, null)
-        FirebaseAuth.getInstance().signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    Log.d("TAG", "signInWithCredential:success")
-                    val user =  FirebaseAuth.getInstance().currentUser
-                    //updateUI(user);
-                } else { // If sign in fails, display a message to the user.
-                    Log.w(
-                        "TAG",
-                        "signInWithCredential:failure",
-                        task.exception
-                    )
-                    Toast.makeText(this@MainActivity, "Authentication Failed.", Toast.LENGTH_SHORT)
-                        .show()
-                    //updateUI(null);
-                }
-            }
-    }
-
-    companion object {
-        private const val RC_SIGN_IN = 2
     }
 }
