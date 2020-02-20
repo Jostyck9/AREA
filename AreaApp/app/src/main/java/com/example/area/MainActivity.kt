5d9486125f4ac8c25bb3fef8f639cdcbfe7d2dc0
Romane <romane.bezier@gmail.com>
2020-02-20 11:15:22 +0100

    Auth with microsoft

--- AreaApp/app/src/main/java/com/example/area/MainActivity.kt
+++ AreaApp/app/src/main/java/com/example/area/MainActivity.kt
@@ -1,22 +1,73 @@
 package com.example.area
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.OAuthProvider
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        //Microsoft button
+        val microsoftButton: CardView = findViewById(R.id.microsoftButton)
+        microsoftButton.setOnClickListener {
+            microsoftAuthentication()
+        }
 
         //Redirection
         val signInRedirection: Button = findViewById(R.id.signInRedirection)
         signInRedirection.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun microsoftAuthentication() {
+        val provider = OAuthProvider.newBuilder("microsoft.com")
+
+        val pendingResultTask: Task<AuthResult>? = auth.pendingAuthResult
+        if (pendingResultTask != null) {
+            pendingResultTask.addOnSuccessListener {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+
+            }.addOnFailureListener {
+                Toast.makeText(applicationContext, "Failure", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            auth.startActivityForSignInWithProvider( /* activity= */this, provider.build())
+                .addOnSuccessListener {
+                    val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+                }
+                .addOnFailureListener {
+                    Toast.makeText(applicationContext, "Failure", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 }
--- AreaApp/app/src/main/java/com/example/area/ProfileActivity.kt
+++ AreaApp/app/src/main/java/com/example/area/ProfileActivity.kt
@@ -3,6 +3,7 @@ package com.example.area
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
@@ -20,6 +21,7 @@ class ProfileActivity : AppCompatActivity() {
 
         profileRedirection.setOnClickListener {
             //TODO déconnecter l'utilisateur
+            FirebaseAuth.getInstance().signOut()
             val i = Intent(applicationContext, MainActivity::class.java)
             i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
