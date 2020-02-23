0d21c0e95ad0c50c96e89ce139c93b40d1373956
gabin warnier de wailly <gabin.warnier-de-wailly@epitech.eu>
2020-02-23 16:43:53 +0100

    +add upgtrade webhooks twitter

--- twitter_webhook/webhooks_twitter.js
+++ twitter_webhook/webhooks_twitter.js
@@ -1,23 +1,4 @@
 const twitterWebhooks = require('twitter-webhooks');
-
-var create_webhook = async function(app) {
-	const userActivityWebhook = twitterWebhooks.userActivity({
-		serverUrl: 'https://3b715a35.ngrok.io',
-		route: '/',
-		consumerKey: 'GKRASjadiIHwSBs9KkO7KXhIM',
-		consumerSecret: '8dlwneANyz6WJTUR8NOBcYkYVSL9jEVviPfWbHoKcmC8ERnYQ9',
-		accessToken: '1098557912677576704-2fz3FvHUaDs5ccaje09f8YhiWpISEn',
-		accessTokenSecret: 'pdymBZU6dt229qycuNSyAo11cN9adU3yb2Nhkrka8CQnX',
-		environment: 'TestArea',
-		app
-	});
-
-	userActivityWebhook.register().catch(err => {
-		console.log(err);
-		return null
-	});
-	return userActivityWebhook
-}
 var add_user_to_twitter_webhook = async function (userActivityWebhook, userId, userToken, secretToken) {
 		userActivityWebhook.subscribe({
 		userId: userId,
@@ -36,9 +17,13 @@ var add_user_to_twitter_webhook = async function (userActivityWebhook, userId, u
 		.on('tweet_delete', (data) => console.log (userActivity.id + ' - tweet_delete'))
 	}).catch(err => {
 		console.log(err)
-		return null
 	});
-	userActivityWebhook.on('event', (event, userId, data) => console.log (userId + ' ' + event + ' ' + data));
-	userActivityWebhook.on('unknown-event', (rawData) => console.log (rawData));
-	return userActivityWebhook
+}
+
+var delete_user_to_twitter_webhook = async function (userActivityWebhook, userId, userToken, secretToken) {
+	userActivityWebhook.unsubscribe({
+		userId: userId,
+		accessToken: userToken,
+		accessTokenSecret: secretToken
+	})
 }
