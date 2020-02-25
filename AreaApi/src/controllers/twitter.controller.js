c1162b3f58f6c98252570895a288b50b3e7ac16b
gabin warnier de wailly <gabin.warnier-de-wailly@epitech.eu>
2020-02-25 23:56:18 +0100

    +add new post tweet

--- AreaApi/src/controllers/twitter.controller.js
+++ AreaApi/src/controllers/twitter.controller.js
@@ -1,4 +1,5 @@
 const twitterWebhooks = require('twitter-webhooks')
+const connector = require('./area.controller')
 
 const CONSUMER_KEY = process.env.TWITTER_API_KEY;
 const CONSUMER_SECRET = process.env.TWITTER_API_SECRET;
@@ -32,17 +33,6 @@ exports.add_user_to_twitter_webhook = async function (userId, userToken, secretT
 		userId: userId,
 		accessToken: userToken,
 		accessTokenSecret: secretToken
-	}).then(function (userActivity) {
-		userActivity
-		.on('favorite', (data) => console.log (userActivity.id + ' - favorite'))
-		.on('tweet_create', (data) => console.log (userActivity.id + ' - tweet_create'))
-		.on('follow', (data) => console.log (userActivity.id + ' - follow'))
-		.on('mute', (data) => console.log (userActivity.id + ' - mute'))
-		.on('revoke', (data) => console.log (userActivity.id + ' - revoke'))
-		.on('direct_message', (data) => console.log (userActivity.id + ' - direct_message'))
-		.on('direct_message_indicate_typing', (data) => console.log (userActivity.id + ' - direct_message_indicate_typing'))
-		.on('direct_message_mark_read', (data) => console.log (userActivity.id + ' - direct_message_mark_read'))
-		.on('tweet_delete', (data) => console.log (userActivity.id + ' - tweet_delete'))
 	}).catch(err => {
 		console.log(err)
 	});
@@ -99,7 +89,13 @@ exports.init_twitter = async function(app) {
 		app
 	});
 	userActivityWebhook.on('event', (event, userId, data) => {
-		console.log (userId + ' ' + event + ' ' + data.text)
+		if (event == 'tweet_create') {
+			const action_result = {
+				user: userId,
+				message: data.text
+			}
+			connector.connectActionToReaction(2, action_result);
+		}
 	});
 }
 
