0786657ef3aa49e651da75396490e4b5b50fdf89
FabienBrosset <fabien.brosset@epitech.eu>
2020-02-25 15:25:14 +0100

    home page change when connected or not

--- AreaWeb/src/pages/Home/index.js
+++ AreaWeb/src/pages/Home/index.js
@@ -11,11 +11,18 @@ export default class Home extends React.Component {
     }
 
     render() {
+        var islogged;
+        if (localStorage.getItem('currentUser'))
+            islogged = <Button variant="secondary" size="lg" active onClick={this.printToken}>Create an Area</Button>
+        else {
+            islogged = <div><h1 class="display-4">Welcome to AREA</h1>
+                        <h5>You need to be connected first to manage your services.</h5></div>
+        }
+
         return (
             <div class="text-center">
-                <h1 class="display-4">Welcome to AREA</h1>
-                <h5>You need to be connected first to manage your services.</h5>
-                <Button variant="secondary" size="lg" active onClick={this.printToken}>Print Token</Button><br/>
+                <br/>
+                {islogged}
                 <footer>
                     <img src={tree} className="tree-logo" alt="tree"/>
                 </footer>
